848025dc7f9f819c9f057e2738adb9672d409195
Bryan Simbaña <bryan-_1997@outlook.com>
2022-12-18 18:21:23 -0500

    Create deck

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+let deck = [];
+const french_suits = ['C', 'D', 'H', 'S'];
+const court_cards = ['A', 'Q', 'J', 'K'];
+
+const createDeck = () => {
+  for (let index = 2; index <= 10; index++) {
+    for (let french_suit of french_suits) {
+      deck.push(index + french_suit);
+    }
+  }
+
+  for (let french_suit of french_suits) {
+    for (let court_card of court_cards) {
+      deck.push(court_card + french_suit);
+    }
+  }
+
+  return deck
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+};
+
+console.log(createDeck());
