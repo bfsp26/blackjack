25d57f8426aa3d770352922981ce9395a2af2750
Bryan Simbaña <bryan-_1997@outlook.com>
2022-12-18 18:39:09 -0500

    Request a card

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ const french_suits = ['C', 'D', 'H', 'S'];
 const court_cards = ['A', 'Q', 'J', 'K'];
 
 const createDeck = () => {
+
   for (let index = 2; index <= 10; index++) {
     for (let french_suit of french_suits) {
       deck.push(index + french_suit);
@@ -21,4 +22,17 @@ const createDeck = () => {
     .map(({ value }) => value);
 };
 
-console.log(createDeck());
+createDeck();
+
+const requestCard = () => {
+
+  if (deck.length === 0) {
+    throw 'There are no cards in the deck.';
+  }
+
+  return deck.pop();
+};
+
+requestCard();
+
+console.log(deck.length);
