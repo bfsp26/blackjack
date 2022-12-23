74998b87f250090e871acdf10da3a37723d48a7a
Bryan Simbaña <bryan-_1997@outlook.com>
2022-12-22 21:03:00 -0500

    Add CPU logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@ const btnReqCard = document.querySelector('#btn-request-card');
 const btnNewGame = document.querySelector('#btn-new-game');
 const btnStopGame = document.querySelector('#btn-stop-game');
 const playerCards = document.querySelector('#player-cards');
+const cpuCards = document.querySelector('#cpu-cards');
 let playerCount = document.querySelector('body > div > div:nth-child(2) > div > h1 > small');
 let cpuCount = document.querySelector('body > div > div:nth-child(3) > div > h1 > small');
 let playerPoints = 0;
@@ -47,6 +48,24 @@ const getValueCard = (card) => {
   return (isNaN(value)) ? (value === 'A') ? 11 : 10 : value * 1;
 };
 
+const cpu = (minPoints) => {
+
+  do {
+    const card = requestCard();
+    cpuPoints = cpuPoints + getValueCard(card);
+    cpuCount.innerHTML = cpuPoints;
+    const imgCard = document.createElement('img');
+    imgCard.src = `assets/cards/${card}.png`;
+    imgCard.classList.add('bj-card');
+    cpuCards.append(imgCard);
+
+    if (minPoints > 21) {
+      break;
+    }
+
+  } while ((cpuPoints < minPoints) && (minPoints < 21));
+};
+
 btnReqCard.addEventListener('click', () => {
   const card = requestCard();
   playerPoints = playerPoints + getValueCard(card);
@@ -59,7 +78,18 @@ btnReqCard.addEventListener('click', () => {
   if (playerPoints > 21) {
     console.warn('You lost!');
     btnReqCard.disabled = true;
+    btnStopGame.disabled = true;
+    cpu(playerPoints);
   } else if (playerPoints === 21) {
     console.info('That\'s great!');
+    btnReqCard.disabled = true;
+    btnStopGame.disabled = true;
+    cpu(playerPoints);
   }
+});
+
+btnStopGame.addEventListener('click', () => {
+  btnReqCard.disabled = true;
+  btnStopGame.disabled = true;
+  cpu(playerPoints);
 });
